d5f5e340fc65b9ac52a590a5e02e97639049cef8
IngridGozner <ingrid_gozner@yahoo.com>
2021-09-18 12:30:48 +0300

    changed to canvas, draw image on canvas

--- public/components/memegenerator.js
+++ public/components/memegenerator.js
@@ -42,31 +42,33 @@ Vue.component('memegenerator', {
       }
   },
   methods: {
-    preview_image() {
-      this.url= URL.createObjectURL(this.image)
+    uploadImage() {
+      this.url= URL.createObjectURL(this.image);
+
+      const c = document.getElementById("memeCanvas");
+      const ctx = c.getContext("2d");
+      const img = new Image;
+
+      // on image load update Canvas Image
+      img.onload = () => {
+        //Clear Old Image and Reset Bounds
+        ctx.clearRect(0, 0, c.width, c.height);
+        c.height = img.height;
+        c.width = img.width;
+
+        // Redraw Image
+        ctx.drawImage(img, 10, 10, c.width, c.height);
+      };
+      img.src = this.url;
+      }
     },
-  },
   template:
   `
   <v-container fluid>
      <v-row>
       <v-col cols="12" md="7">
-         <v-card elevation="2" width="900" color="rgba(0, 0, 0, 0.6)">
-          <v-img id="meme" :src="url" :lazy-src="url">
-
-            <v-container style="position:absolute;top:0px">
-              <v-row align="center" justify="space-around">
-                <div v-bind:style="{ fontSize: topSize + 'px', color: topColor, fontFamily: topFont }">{{ topText }}</div>
-              </v-row>
-            </v-container>
-
-            <v-container style="position:absolute;bottom:0px">
-              <v-row align="center" justify="space-around">
-                <div v-bind:style="{ fontSize: bottomSize + 'px', color: bottomColor, fontFamily: bottomFont }">{{ bottomText }}</div>
-              </v-row>
-            </v-container>
-
-          </v-img>
+         <v-card elevation="2">
+         <canvas id="memeCanvas" class="fullwidth" width="750px" height="620px"></canvas>
         </v-card>
 
       </v-col>
@@ -76,7 +78,7 @@ Vue.component('memegenerator', {
          <v-file-input
             label="Upload Image"
             accept="image/*"
-            @change="preview_image"
+            @change="uploadImage"
             v-model="image"
             outlined
             filled
@@ -185,7 +187,7 @@ Vue.component('memegenerator', {
           </v-col></v-row>
 
           </v-card-text>
-          <v-card-actions><slot></slot></v-card>
+          <v-card-actions><slot></slot></v-card-actions>
         </v-card>
       </v-col>
     </v-row>
